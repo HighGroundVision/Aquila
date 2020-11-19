cd6ad5fc857c31d06c5a1788013447b2bfcbcc26
RGBKnights <master@rgbknights.com>
2020-11-19 15:09:19 -0500

    Imported Project

---
+++ src/Functions/HeroesFunction.cs
@@ -0,0 +1,77 @@
+using HGV.Aquila.Models;
+using HGV.Basilius.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Polly;
+using Polly.Registry;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HGV.Aquila.Functions
+{
+    public class HeroesFunction
+    {
+        private readonly IMetaClient metaClient;
+        private readonly HttpClient httpClient;
+        private readonly IAsyncPolicy<string> cachePolicy;
+
+        public HeroesFunction(HttpClient httpClient, IMetaClient metaClient, IReadOnlyPolicyRegistry<string> policyRegistry)
+        {
+            this.httpClient = httpClient;
+            this.metaClient = metaClient;
+            this.cachePolicy = policyRegistry.Get<IAsyncPolicy<string>>("GetHeroes");
+        }
+
+        [FunctionName("Heroes")]
+        public async Task<IActionResult> GetHeroes(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "heroes")] HttpRequest req,
+            ILogger log)
+        { 
+            var policy = await this.cachePolicy.ExecuteAndCaptureAsync(() => 
+                this.httpClient.GetStringAsync("http://abilitydraft.datdota.com/api/heroes")
+            );
+
+            var result = JsonConvert.DeserializeObject<Root>(policy.Result);
+            var major = result.Data.Patches.Majors.Last();
+            var patch = result.Data.Patches.Details[major].FirstOrDefault();
+            var stats = result.Data.Heroes.Select(_ => new { Id = _.Key, Data = _.Value[patch] }).ToList();
+            var meta = this.metaClient.GetHeroes().Where(_ => _.AbilityDraftEnabled == true).ToList();
+
+            var collection = meta
+                .Join(stats, _ => _.Id, _ => _.Id, (lhs, rhs) => new Hero() 
+                { 
+                    Id = lhs.Id,
+                    Name = lhs.Name,
+                    Key = lhs.Key.Replace("npc_dota_hero_", ""),
+                    Aliases = lhs.NameAliases,
+                    ImageBanner = lhs.ImageBanner,
+                    ImageIcon = lhs.ImageIcon,
+                    ImageProfile = lhs.ImageProfile,
+                    AbilityReplaceRequired = lhs.AbilityReplaceRequired,
+                    StrengthGain = lhs.AttributeStrengthGain,
+                    MaxStrength = lhs.AttributeBaseStrength + (lhs.AttributeStrengthGain * 30),
+                    IntelligenceGain = lhs.AttributeIntelligenceGain,
+                    MaxIntelligence= lhs.AttributeBaseIntelligence + (lhs.AttributeIntelligenceGain * 30),
+                    AgilityGain = lhs.AttributeAgilityGain,
+                    MaxAgility = lhs.AttributeBaseAgility + (lhs.AttributeAgilityGain * 30),
+                    AttributePrimary = lhs.AttributePrimary,
+                    AttackCapabilities = lhs.AttackCapabilities,
+                    Damage = (lhs.AttackDamageMin + lhs.AttackDamageMax) / 2,
+                    Roles = lhs.Roles,
+                    WinRate = rhs.Data.WinRate,
+                    Picked = false,
+                })
+                .OrderBy(_ => _.Name)
+                .ToList();
+
+            return new OkObjectResult(collection);
+        }
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,35 @@
+﻿using HGV.Basilius.Client;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Caching;
+using Polly.Registry;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(HGV.Aquila.Startup))]
+
+namespace HGV.Aquila
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Singleton
+            builder.Services.AddHttpClient();
+            builder.Services.AddSingleton<IMetaClient>(new MetaClient());
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSingleton<Polly.Caching.IAsyncCacheProvider, Polly.Caching.Memory.MemoryCacheProvider>();
+
+            builder.Services.AddSingleton<Polly.Registry.IReadOnlyPolicyRegistry<string>, Polly.Registry.PolicyRegistry>((serviceProvider) =>
+            {
+                var registry = new PolicyRegistry();
+                var provider = serviceProvider.GetRequiredService<IAsyncCacheProvider>().AsyncFor<string>();
+                registry.Add("GetHeroes", Policy.CacheAsync<string>(provider,TimeSpan.FromMinutes(5)));
+                return registry;
+            });
+        }
+    }
+}

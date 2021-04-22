fe253274cc51e5bc8b36164f505e7e8aa1369695
RGBKnights <master@rgbknights.com>
2021-04-22 16:20:24 -0400

    updated the SD functions

--- src/Functions/SingleDraftFunction.cs
+++ src/Functions/SingleDraftFunction.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace HGV.Aquila
@@ -18,6 +19,243 @@ namespace HGV.Aquila
         {
         }
 
+        [FunctionName("SingleDraftNegotiate")]
+        public async Task<SignalRConnectionInfo> Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "single/{id}/negotiate")] HttpRequest req,
+            string id,
+            IBinder binder)
+        {
+            var attr = new SignalRConnectionInfoAttribute()
+            {
+                ConnectionStringSetting = "SignalRConnectionString",
+                HubName = "SingleDraft",
+                UserId = id,
+            };
+            var connectionInfo = await binder.BindAsync<SignalRConnectionInfo>(attr);
+            return connectionInfo;
+        }
+
+        [FunctionName("SingleDraftCreateLobby")]
+        public async Task<IActionResult> Create(
+             [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<JObject> collector,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "single")] HttpRequest req
+        )
+        {
+            var textReader = new StreamReader(req.Body);
+            var jsonReader = new JsonTextReader(textReader);
+            var token = await JObject.LoadAsync(jsonReader);
+
+            var id = Guid.NewGuid();
+            token.Add("id", id);
+            await collector.AddAsync(token);
+            return new CreatedResult($"/single/{id}", new { id });
+        }
+
+        [FunctionName("SingleDraftSaveLobby")]
+        public async Task<IActionResult> Save(
+             [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<JObject> collector,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "single/{id}")] HttpRequest req,
+            string id,
+            [SignalR(HubName = "SingleDraft", ConnectionStringSetting = "SignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRMessages
+        )
+        {
+            var textReader = new StreamReader(req.Body);
+            var jsonReader = new JsonTextReader(textReader);
+            var token = await JObject.LoadAsync(jsonReader);
+
+            await collector.AddAsync(token);
+
+            var msg = new SignalRMessage
+            {
+                UserId = id,
+                Target = "update",
+                Arguments = new object[] {}
+            };
+            await signalRMessages.AddAsync(msg);
+
+            return new OkResult();
+        }
+
+        [FunctionName("SingleDraftGetLobby")]
+        public IActionResult Get(
+             [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                Id = "{id}",
+                PartitionKey = "{id}")] JObject document,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "single/{id}")] HttpRequest req
+
+        )
+        {
+            return new OkObjectResult(document);
+        }
+
+        [FunctionName("SingleDraftClaimSlot")]
+        public async Task<IActionResult> ClaimSlot(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "single/{id}/claim")] HttpRequest req,
+            string id,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                Id = "{id}",
+                PartitionKey = "{id}")] JObject document,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<JObject> collector,
+            [SignalR(HubName = "SingleDraft", ConnectionStringSetting = "SignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRMessages
+        )
+        {
+            var textReader = new StreamReader(req.Body);
+            var jsonReader = new JsonTextReader(textReader);
+            var body = await JObject.LoadAsync(jsonReader);
+
+            var slots = document["slots"] as JArray;
+            var slot = slots.Where(item => item["slot"].Value<int>() == body["slot"].Value<int>() && item["state"].Value<int>() == 0).FirstOrDefault();
+            if(slot == null)
+                return new BadRequestObjectResult("Slot is already taken.");
+
+            slot["name"] = body["name"];
+            slot["state"] = 1;
+
+            await collector.AddAsync(document);
+
+            var msg = new SignalRMessage
+            {
+                UserId = id,
+                Target = "update",
+                Arguments = new object[] {}
+            };
+            await signalRMessages.AddAsync(msg);
+
+            return new OkResult();
+        }
+
+        [FunctionName("SingleDraftReleaseSlot")]
+        public async Task ReleaseSlot(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "single/{id}/release")] HttpRequest req,
+            string id,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                Id = "{id}",
+                PartitionKey = "{id}")] JObject document,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<JObject> collector,
+            [SignalR(HubName = "SingleDraft", ConnectionStringSetting = "SignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRMessages
+        )
+        {
+            var textReader = new StreamReader(req.Body);
+            var jsonReader = new JsonTextReader(textReader);
+            var body = await JObject.LoadAsync(jsonReader);
+
+            var slots = document["slots"] as JArray;
+            foreach (JObject item in slots)
+            {
+                if(item["slot"].Value<int>() == body["slot"].Value<int>())
+                {
+                    item["name"] = string.Empty;
+                    item["state"] = 0;
+                    item["choice"] = null;
+                }
+            }
+            
+            await collector.AddAsync(document);
+
+            var msg = new SignalRMessage
+            {
+                UserId = id,
+                Target = "update",
+                Arguments = new object[] {}
+            };
+            await signalRMessages.AddAsync(msg);
+        }
+
+        
+        [FunctionName("SingleDraftReady")]
+        public async Task<IActionResult> Ready(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "single/{id}/ready")] HttpRequest req,
+            string id,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                Id = "{id}",
+                PartitionKey = "{id}")] JObject document,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<JObject> collector,
+            [SignalR(HubName = "SingleDraft", ConnectionStringSetting = "SignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRMessages
+        )
+        {
+            document["ready"] = true;
+
+            await collector.AddAsync(document);
+
+            var msg = new SignalRMessage
+            {
+                UserId = id,
+                Target = "update",
+                Arguments = new object[] {}
+            };
+            await signalRMessages.AddAsync(msg);
+
+            return new OkResult();
+        }
+
+        [FunctionName("SingleDraftChoice")]
+        public async Task<IActionResult> Choice(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "single/{id}/choice")] HttpRequest req,
+            string id,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                Id = "{id}",
+                PartitionKey = "{id}")] JObject document,
+            [CosmosDB(
+                databaseName: "hgv-aquila",
+                collectionName: "single-draft",
+                ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<JObject> collector,
+            [SignalR(HubName = "SingleDraft", ConnectionStringSetting = "SignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRMessages
+        )
+        {
+            var textReader = new StreamReader(req.Body);
+            var jsonReader = new JsonTextReader(textReader);
+            var body = await JObject.LoadAsync(jsonReader);
+
+            var slots = document["slots"] as JArray;
+            var slot = slots.Where(item => item["slot"].Value<int>() == body["slot"].Value<int>()).FirstOrDefault();
+        
+            slot["choice"] = body["choice"];
+            slot["state"] = 2;
+
+            await collector.AddAsync(document);
+
+            var msg = new SignalRMessage
+            {
+                UserId = id,
+                Target = "update",
+                Arguments = new object[] {}
+            };
+            await signalRMessages.AddAsync(msg);
+
+            return new OkResult();
+        }
+
+        /*
         [FunctionName("SingleDraftNegotiate")]
         public async Task<SignalRConnectionInfo> Negotiate(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "single/{session}/negotiate")] HttpRequest req,
@@ -103,5 +341,6 @@ namespace HGV.Aquila
                     Arguments = new[] { body.slot, body.key }
                 });
         }
+        */
     }
 }
